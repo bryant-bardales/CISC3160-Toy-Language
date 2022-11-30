d0312393207b1e5965b4ab16622d7c85acfeba72
Bryant Bardales <bryant.bardales@gmail.com>
2022-11-30 11:17:20 -0500

    Add methods to the evaluator to check for semi-colon, parenthesis, operator, and variable initializer

--- ToyLangEval.java
+++ ToyLangEval.java
@@ -28,4 +28,65 @@ public class ToyLangEval {
 
         inputToken = c;
     }
+
+     // Checks that the assignment ends with a semicolon.
+     int eval() {
+        int x = addOrSubtract();
+        if (inputToken == ';') {
+            return x;
+        } else {
+            throw new RuntimeException("Missing ';' token expected (2)");
+        }
+    }
+
+    // Checks for a closing parenthesis.
+    void checkParenth(char token) {
+        if (inputToken == token) {
+            nxtToken();
+        } else {
+            throw new RuntimeException("Missing Parenthesis");
+        }
+    }
+
+    void ToyLangEval(String s) {
+        this.s = s.replaceAll("\\s", ""); // Removes all empty spaces inside the string.
+        currIdx = 0;
+        nxtToken();
+    }
+
+    // Initializes the variable and the value assigned to the variable
+    void declareVar() {
+
+        String var = iD(); // Gets the sB.toString(), the variable
+        int valueOrOperand = eval(); // Gets the value or the operand
+        hMap.put(var, valueOrOperand); // Stores the var and operand into hashmap
+        System.out.println("Output:");
+        System.out.println(var + " = " + valueOrOperand);
+
+    }
+
+    // In the case the operator is addition or subtraction
+    int addOrSubtract() {
+        int x = multiplyOrDivide();
+        while (inputToken == '+' || inputToken == '-') {
+            char op = inputToken;
+            nxtToken();
+            int y = multiplyOrDivide();
+            x = compute(op, x, y);
+        }
+        return x;
+    }
+
+    // In the case the operator is multiplication or division
+    int multiplyOrDivide() {
+        int x = factor();
+        while (inputToken == '*' || inputToken == '/') {
+            char op = inputToken;
+            nxtToken();
+            int y = factor();
+            x = compute(op, x, y);
+        }
+        return x;
+    }
+
 }
